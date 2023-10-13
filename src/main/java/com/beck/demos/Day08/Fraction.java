65c5f5dadf9247c2d4df15cdd22e018328030366
Jonathan Beck <JJBeck7@gmail.com>
2023-10-13 12:14:04 -0500

    Partner Project 3 - Adding my own version of the "simplify" method

--- src/main/java/com/beck/demos/Day08/Fraction.java
+++ src/main/java/com/beck/demos/Day08/Fraction.java
@@ -58,16 +58,36 @@ public class Fraction {
     int gcd = Integer.parseInt(i.toString());
     return gcd;
   }
-  public Fraction simplify() {
+
+  //removing instructor code (as to ad in my own code later)
+
+  //public Fraction simplify() {
+   // int gcd = greatestCommonDivisor(this.numerator, this.denominator);
+    //Fraction simplifiedFraction = new Fraction(this.numerator / gcd, this.denominator / gcd);
+    //if(simplifiedFraction.numerator >= 0 && simplifiedFraction.denominator < 0
+     //   || simplifiedFraction.numerator < 0 && simplifiedFraction.denominator < 0) {
+     // simplifiedFraction.numerator *= -1;
+     // simplifiedFraction.denominator *= -1;
+   // }
+    //return simplifiedFraction;
+ // }
+
+  //Adding in my code from last term to see if the tests still pass
+  public Fraction simplify(){
     int gcd = greatestCommonDivisor(this.numerator, this.denominator);
-    Fraction simplifiedFraction = new Fraction(this.numerator / gcd, this.denominator / gcd);
-    if(simplifiedFraction.numerator >= 0 && simplifiedFraction.denominator < 0
-        || simplifiedFraction.numerator < 0 && simplifiedFraction.denominator < 0) {
-      simplifiedFraction.numerator *= -1;
-      simplifiedFraction.denominator *= -1;
+    int newNumerator = this.numerator / gcd;
+    int newDenominator = this.denominator / gcd;
+    if (newNumerator >= 0 && newDenominator < 0){
+      newNumerator = newNumerator * -1;
+      newDenominator = newDenominator * -1;
+    } else if (newNumerator < 0 && newDenominator < 0){
+      newNumerator = newNumerator * -1;
+      newDenominator = newDenominator * -1;
     }
-    return simplifiedFraction;
+    Fraction f = new Fraction(newNumerator, newDenominator);
+    return f;
   }
+
 
 }
 
